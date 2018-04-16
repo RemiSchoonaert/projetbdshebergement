f4b8456a616121124ccd967253d79b18cc7878e7
RemiSchoonaert <32642662+RemiSchoonaert@users.noreply.github.com>
2018-04-17 01:51:26 +0200

    Update DataSourceProvider.java

--- src/main/java/bs/devweb/projet/provider/DataSourceProvider.java
+++ src/main/java/bs/devweb/projet/provider/DataSourceProvider.java
@@ -16,11 +16,11 @@ public class DataSourceProvider {
     public static PGSimpleDataSource getDataSource() {
         if (dataSource == null) {
         	dataSource = new PGSimpleDataSource();
-            dataSource.setURL("jdbc:postgresql://ec2-79-125-117-53.eu-west-1.compute.amazonaws.com:5432/d98khftjj2v0co?user=axnsoltojbwnvm&password=32341629078836716800d2f8a15f500c65554b3dc66cee3cb8143abb50b4dbe5&sslmode=require");
-            dataSource.setDatabaseName("d98khftjj2v0co");
-            dataSource.setUser("axnsoltojbwnvm");
-            dataSource.setPortNumber(5432);
-            dataSource.setPassword("32341629078836716800d2f8a15f500c65554b3dc66cee3cb8143abb50b4dbe5");
+            dataSource.setURL("");
+            dataSource.setDatabaseName("");
+            dataSource.setUser("");
+            dataSource.setPortNumber();
+            dataSource.setPassword("");
         }
         return dataSource;
     }
